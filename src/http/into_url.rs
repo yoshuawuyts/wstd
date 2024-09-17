82badb3f1ed76e14932b08dca755a8a30c982773
Calvin Prewitt <calvin@jaflabs.com>
2024-09-16 20:44:37 -0500

    revised

--- src/http/into_url.rs
+++ src/http/into_url.rs
@@ -2,10 +2,10 @@ use url::Url;
 
 use super::Result;
 
-/// A trait to try to convert some type into a `Url`.
+/// A trait for converting types into a `Url`.
 ///
-/// This trait is "sealed", such that only types within reqwest can
-/// implement it.
+/// This trait is "sealed" to limit implementations
+/// to this crate.
 pub trait IntoUrl: IntoUrlSealed {}
 
 impl IntoUrl for Url {}
@@ -14,10 +14,7 @@ impl<'a> IntoUrl for &'a str {}
 impl<'a> IntoUrl for &'a String {}
 
 pub trait IntoUrlSealed {
-    // Besides parsing as a valid `Url`, the `Url` must be a valid
-    // `http::Uri`, in that it makes sense to use in a network request.
     fn into_url(self) -> Result<Url>;
-
     fn as_str(&self) -> &str;
 }
 
@@ -60,29 +57,5 @@ impl IntoUrlSealed for String {
 
     fn as_str(&self) -> &str {
         self.as_ref()
-    }
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-    use std::error::Error;
-
-    #[test]
-    fn into_url_file_scheme() {
-        let err = "file:///etc/hosts".into_url().unwrap_err();
-        assert_eq!(
-            err.source().unwrap().to_string(),
-            "URL scheme is not allowed"
-        );
-    }
-
-    #[test]
-    fn into_url_blob_scheme() {
-        let err = "blob:https://example.com".into_url().unwrap_err();
-        assert_eq!(
-            err.source().unwrap().to_string(),
-            "URL scheme is not allowed"
-        );
     }
 }
