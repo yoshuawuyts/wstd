76a073374961bba67c1b9d0ee6ea18ba0b95eabc
Pat Hickey <pat@moreproductive.org>
2024-11-20 19:50:39 -0800

    use content-length driven Body::len in http_get.

--- examples/http_get.rs
+++ examples/http_get.rs
@@ -1,5 +1,5 @@
 use std::error::Error;
-use wstd::http::{Client, HeaderValue, Method, Request};
+use wstd::http::{Body, Client, HeaderValue, Method, Request};
 use wstd::io::AsyncRead;
 
 #[wstd::main]
@@ -17,8 +17,19 @@ async fn main() -> Result<(), Box<dyn Error>> {
         .ok_or_else(|| "response expected to have Content-Type header")?;
     assert_eq!(content_type, "application/json; charset=utf-8");
 
+    let body = response.body();
+    let body_len = body
+        .len()
+        .ok_or_else(|| "GET postman-echo.com/get is supposed to provide a content-length")?;
+
     let mut body_buf = Vec::new();
-    let _body_len = response.body().read_to_end(&mut body_buf).await?;
+    body.read_to_end(&mut body_buf).await?;
+
+    assert_eq!(
+        body_buf.len(),
+        body_len,
+        "read_to_end length should match content-length"
+    );
 
     let val: serde_json::Value = serde_json::from_slice(&body_buf)?;
     let body_url = val
