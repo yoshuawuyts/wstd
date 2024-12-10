6cf89c898ed1ae2d5d37e92ff7baeec59be3ad3d
Pat Hickey <pat@moreproductive.org>
2024-12-09 16:01:36 -0800

    inline trivial sleep and sleep_until into task.rs

--- src/task/sleep.rs
+++
@@ -1,8 +0,0 @@
-use crate::time::{Duration, Timer, Wait};
-
-/// Sleeps for the specified amount of time.
-///
-/// This future can be `push_deadline` to be moved
-pub fn sleep(dur: Duration) -> Wait {
-    Timer::after(dur).wait()
-}
--- src/task/sleep_until.rs
+++
@@ -1,6 +0,0 @@
-use crate::time::{Instant, Timer, Wait};
-
-/// Sleeps until the specified instant.
-pub fn sleep_until(deadline: Instant) -> Wait {
-    Timer::at(deadline).wait()
-}
