907e883f5c0a93a4b73625cfe789b1dfd6f5fe68
Dan Gohman <dev@sunfishcode.online>
2025-01-13 07:16:14 -0800

    Delete `InvalidHeader`, which was unused.

--- src/http/fields.rs
+++ src/http/fields.rs
@@ -1,9 +1,6 @@
 pub use http::header::{HeaderMap, HeaderName, HeaderValue};
-use http::header::{InvalidHeaderName, InvalidHeaderValue};
 
-use super::error::ErrorVariant;
 use super::{Error, Result};
-use std::fmt;
 use wasi::http::types::Fields;
 
 pub(crate) fn header_map_from_wasi(wasi_fields: Fields) -> Result<HeaderMap> {
@@ -27,42 +24,4 @@ pub(crate) fn header_map_to_wasi(header_map: &HeaderMap) -> Fields {
             .unwrap_or_else(|err| panic!("header named {key}: {err:?}"));
     }
     wasi_fields
-}
-
-#[derive(Debug)]
-pub(crate) enum InvalidHeader {
-    Name(InvalidHeaderName),
-    Value(InvalidHeaderValue),
-}
-
-impl fmt::Display for InvalidHeader {
-    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        match self {
-            Self::Name(e) => e.fmt(f),
-            Self::Value(e) => e.fmt(f),
-        }
-    }
-}
-
-impl std::error::Error for InvalidHeader {}
-
-impl From<InvalidHeaderName> for InvalidHeader {
-    fn from(e: InvalidHeaderName) -> Self {
-        Self::Name(e)
-    }
-}
-
-impl From<InvalidHeaderValue> for InvalidHeader {
-    fn from(e: InvalidHeaderValue) -> Self {
-        Self::Value(e)
-    }
-}
-
-impl From<InvalidHeader> for Error {
-    fn from(e: InvalidHeader) -> Self {
-        match e {
-            InvalidHeader::Name(e) => ErrorVariant::HeaderName(e).into(),
-            InvalidHeader::Value(e) => ErrorVariant::HeaderValue(e).into(),
-        }
-    }
 }
