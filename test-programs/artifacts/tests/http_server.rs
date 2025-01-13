a8358de39eb627e22ac8a5e7add5307f121ac09f
Dan Gohman <dev@sunfishcode.online>
2025-01-13 07:13:25 -0800

    Spawn the wasmtime executable rather than using wasmtime_cli.

--- test-programs/artifacts/tests/http_server.rs
+++ test-programs/artifacts/tests/http_server.rs
@@ -1,25 +1,5 @@
 use anyhow::Result;
-
-fn run_in_wasmtime(wasm: &str) -> Result<()> {
-    use clap::Parser;
-    use wasmtime_cli::commands::ServeCommand;
-
-    // Run wasmtime serve.
-    // Enable -Scli because we build with the default adapter rather than the
-    // proxy adapter.
-    // Disable logging so that Wasmtime's tracing_subscriber registration
-    // doesn't conflict with the test harness' registration.
-    let serve =
-        match ServeCommand::try_parse_from(["serve", "-Scli", "-Dlogging=n", wasm].into_iter()) {
-            Ok(serve) => serve,
-            Err(e) => {
-                dbg!(&e);
-                return Err(e.into());
-            }
-        };
-
-    serve.execute()
-}
+use std::process::Command;
 
 #[test_log::test]
 fn http_server() -> Result<()> {
@@ -27,24 +7,30 @@ fn http_server() -> Result<()> {
     use std::thread::sleep;
     use std::time::Duration;
 
-    // Start a `wasmtime serve` server.
-    let wasmtime_thread =
-        std::thread::spawn(move || run_in_wasmtime(test_programs_artifacts::HTTP_SERVER));
+    // Run wasmtime serve.
+    // Enable -Scli because we currently don't have a way to build with the
+    // proxy adapter, so we build with the default adapter.
+    let mut wasmtime_process = Command::new("wasmtime")
+        .arg("serve")
+        .arg("-Scli")
+        .arg("--addr=127.0.0.1:8081")
+        .arg(test_programs_artifacts::HTTP_SERVER)
+        .spawn()?;
 
     // Clumsily wait for the server to accept connections.
     'wait: loop {
         sleep(Duration::from_millis(100));
-        if TcpStream::connect("127.0.0.1:8080").is_ok() {
+        if TcpStream::connect("127.0.0.1:8081").is_ok() {
             break 'wait;
         }
     }
 
     // Do some tests!
 
-    let body: String = ureq::get("http://127.0.0.1:8080").call()?.into_string()?;
+    let body: String = ureq::get("http://127.0.0.1:8081").call()?.into_string()?;
     assert_eq!(body, "Hello, wasi:http/proxy world!\n");
 
-    match ureq::get("http://127.0.0.1:8080/fail").call() {
+    match ureq::get("http://127.0.0.1:8081/fail").call() {
         Ok(body) => {
             unreachable!("unexpected success from /fail: {:?}", body);
         }
@@ -56,7 +42,7 @@ fn http_server() -> Result<()> {
 
     const MESSAGE: &[u8] = b"hello, echoserver!\n";
 
-    let body: String = ureq::get("http://127.0.0.1:8080/echo")
+    let body: String = ureq::get("http://127.0.0.1:8081/echo")
         .send(MESSAGE)?
         .into_string()?;
     assert_eq!(body.as_bytes(), MESSAGE);
@@ -70,7 +56,7 @@ fn http_server() -> Result<()> {
         ("Purple", "Beets"),
     ];
 
-    let mut response = ureq::get("http://127.0.0.1:8080/echo-headers");
+    let mut response = ureq::get("http://127.0.0.1:8081/echo-headers");
     for (name, value) in test_headers {
         response = response.set(name, value);
     }
@@ -81,9 +67,7 @@ fn http_server() -> Result<()> {
         assert_eq!(response.header(name), Some(value));
     }
 
-    if wasmtime_thread.is_finished() {
-        wasmtime_thread.join().expect("wasmtime panicked")?;
-    }
+    wasmtime_process.kill()?;
 
     Ok(())
 }
