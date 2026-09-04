fcb4472259cef0a583c3b642dffaa855cdfe6ac4
Ryan P <r.pridgeon@f5.com>
2026-09-04 13:37:06 -0400

    feat: UdpSocket/UdpStream

--- src/net/mod.rs
+++ src/net/mod.rs
@@ -1,13 +1,15 @@
 //! Async network abstractions.
 
 use std::io::{self, ErrorKind};
-use wasip2::sockets::network::ErrorCode;
+use wasip2::sockets::network::{ErrorCode, IpSocketAddress, Ipv4SocketAddress};
 
 mod tcp_listener;
 mod tcp_stream;
+mod udp;
 
 pub use tcp_listener::*;
 pub use tcp_stream::*;
+pub use udp::*;
 
 fn to_io_err(err: ErrorCode) -> io::Error {
     match err {
@@ -24,6 +26,55 @@ fn to_io_err(err: ErrorCode) -> io::Error {
         ErrorCode::ConnectionReset => ErrorKind::ConnectionReset.into(),
         ErrorCode::ConnectionAborted => ErrorKind::ConnectionAborted.into(),
         ErrorCode::ConcurrencyConflict => ErrorKind::AlreadyExists.into(),
+        ErrorCode::DatagramTooLarge => ErrorKind::InvalidInput.into(),
         _ => ErrorKind::Other.into(),
+    }
+}
+
+fn sockaddr_from_wasi(addr: IpSocketAddress) -> std::net::SocketAddr {
+    use wasip2::sockets::network::Ipv6SocketAddress;
+    match addr {
+        IpSocketAddress::Ipv4(Ipv4SocketAddress { address, port }) => {
+            std::net::SocketAddr::V4(std::net::SocketAddrV4::new(
+                std::net::Ipv4Addr::new(address.0, address.1, address.2, address.3),
+                port,
+            ))
+        }
+        IpSocketAddress::Ipv6(Ipv6SocketAddress {
+            address,
+            port,
+            flow_info,
+            scope_id,
+        }) => std::net::SocketAddr::V6(std::net::SocketAddrV6::new(
+            std::net::Ipv6Addr::new(
+                address.0, address.1, address.2, address.3, address.4, address.5, address.6,
+                address.7,
+            ),
+            port,
+            flow_info,
+            scope_id,
+        )),
+    }
+}
+
+fn sockaddr_to_wasi(addr: std::net::SocketAddr) -> IpSocketAddress {
+    use wasip2::sockets::network::Ipv6SocketAddress;
+    match addr {
+        std::net::SocketAddr::V4(addr) => {
+            let ip = addr.ip().octets();
+            IpSocketAddress::Ipv4(Ipv4SocketAddress {
+                address: (ip[0], ip[1], ip[2], ip[3]),
+                port: addr.port(),
+            })
+        }
+        std::net::SocketAddr::V6(addr) => {
+            let ip = addr.ip().segments();
+            IpSocketAddress::Ipv6(Ipv6SocketAddress {
+                address: (ip[0], ip[1], ip[2], ip[3], ip[4], ip[5], ip[6], ip[7]),
+                port: addr.port(),
+                flow_info: addr.flowinfo(),
+                scope_id: addr.scope_id(),
+            })
+        }
     }
 }
